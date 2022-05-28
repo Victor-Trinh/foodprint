47d2cf96dc6461f6c357c59b05a556e46b23c4e6
Victor-Trinh <trinhvictor12@gmail.com>
2022-05-28 18:42:15 -0400

    update backend with new endpoint for recipes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import ast
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS, cross_origin # source https://stackoverflow.com/questions/25594893/how-to-enable-cors-in-flask
 from dotenv import load_dotenv
 from gvision import GVisionManager
@@ -15,16 +15,28 @@ gvision = GVisionManager()
 @app.route('/')
 @cross_origin()
 def hello(): # replace thing to send webpage
-    return 'Hello, World!' 
+    return send_from_directory("../frontend", "index.html")
 
-@app.route('/foodprint', methods=['POST', 'GET'])
+@app.route('/foodprint-image', methods=['POST', 'GET'])
 @cross_origin()
-def run_inference():
+def foodprint_image():
     save_dir = os.path.join(os.getcwd(), "images")
     save_path = os.path.join(save_dir, str(len(os.listdir(save_dir))) + ".png")
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
-    img = request.files['media'].save(save_path) # MAKE SURE THE IMAGES ARE PNGS
+
+    img = request.files['ReceiptImage'].save(save_path) # MAKE SURE THE IMAGES ARE PNGS
     
     words = [d.description for d in gvision.detect_text(save_path)]
-    return "testresponse"
+    print(words)
+
+    return str(words) # TODO: return food emissions
+
+
+@app.route('/foodprint-recipe', methods=['POST', 'GET'])
+@cross_origin()
+def foodprint_recipe():
+
+    recipeText = request.form["RecipeText"]
+
+    return recipeText # TODO: return food emissions
