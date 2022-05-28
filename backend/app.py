4ef92e32c2408dff4249fac41c7b05300847fdb1
Victor-Trinh <trinhvictor12@gmail.com>
2022-05-28 14:30:03 -0400

    added endpoint to save image

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import ast
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin # source https://stackoverflow.com/questions/25594893/how-to-enable-cors-in-flask
 from dotenv import load_dotenv
@@ -13,10 +14,13 @@ cors = CORS(app)
 def hello(): # replace thing to send webpage
     return 'Hello, World!' 
 
-@app.route('/foodprint', methods=['POST'])
+@app.route('/foodprint', methods=['POST', 'GET'])
 @cross_origin()
 def run_inference():
-    img = ast.literal_eval(request.get_data().decode('utf8'))['img']
+    save_dir = os.path.join(os.getcwd(), "images")
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    img = request.files['media'].save(os.path.join(save_dir, str(len(os.listdir(save_dir))) + ".png")) # MAKE SURE THE IMAGES ARE PNGS
     response = None
 
-    return response
+    return "response"
