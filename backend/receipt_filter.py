1bec3561904296a0613f8b40c754e50a1ad7b23c
ericlin1230 <erichungyu.lin@mail.utoronto.ca>
2022-05-28 19:09:07 -0400

    Added plots for plotly and receipt filter function

---
+++ backend/receipt_filter.py
@@ -0,0 +1,10 @@
+def filter(receipt):
+    """Filter out the food from the list of strings of the receipts
+
+    Args:
+        receipt (Array): An array of strings taken from the receipts
+
+    Returns:
+        _type_: A list of food filtered from the strings
+    """
+    return ['vegetables', 'banana', 'potato', ' vegetables', 'vegetables', 'grapes', 'vegetables', 'vegetables']
